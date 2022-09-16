808ae52a50c00f9a3f30d6e506fd793304d108f2
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-16 14:38:57 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        TestClass test = new TestClass();
+        org.example.TestClass test = new TestClass();
         String string = test.test("kaas");
 
         System.out.println(string);
