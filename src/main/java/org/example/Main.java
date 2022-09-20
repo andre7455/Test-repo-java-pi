da78b9f09253afabbdf4f02fc99eaa2e88eedda2
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-20 09:04:04 +0200

    test kaas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         TestClass test = new TestClass();
+        System.out.println(test.test("x"));
         String string = test.test("kaas");
 
         System.out.println(string);
