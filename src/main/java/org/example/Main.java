779710effddddc0fe10c6280ce1be48004569a34
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-21 23:46:22 +0200

    i made a jar, now it works but i need to update my pi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         TestClass test = new TestClass();
         System.out.println(test.test("x"));
-        String string = test.test("kaas");
+        String string = test.test("kroket");
 
         System.out.println(string);
     }
